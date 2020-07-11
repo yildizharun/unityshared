0cb4499a1484f409a231d62b5147f8a9ebe37b98
yildizharun <yildizharun11@gmail.com>
2020-07-11 23:36:10 +0300

    Add files via upload

---
+++ Scripts/InputManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager :MonoBehaviour
+{
+    private static InputManager instance;
+
+    public static InputManager Instance{
+        get
+        {
+            return instance;
+        }
+    }
+    private void Awake() 
+    {
+        if(Instance==null)
+        {
+            instance=this;
+        }
+    }
+
+    public float getXDirection()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        return x;
+    }
+}
---
+++ Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rigidbody;
+    public float speed;
+    public float jumpForce;
+  
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            jump();
+        }
+    }
+    void FixedUpdate()
+    {
+        move();
+        if(rigidbody.velocity.y<0)
+        {
+            rigidbody.gravityScale=5;
+        }
+        else
+        {
+            rigidbody.gravityScale=1f;
+        }
+        
+    }
+
+    void move()
+    {
+        float xDirectionValue = InputManager.Instance.getXDirection();
+        rigidbody.velocity = new Vector2(xDirectionValue * speed, rigidbody.velocity.y);
+    }
+
+    void jump()
+    { 
+        rigidbody.velocity = new Vector2(rigidbody.velocity.x,0);
+        rigidbody.velocity+=Vector2.up*jumpForce; 
+    }
+}
